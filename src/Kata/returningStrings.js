b172b911871ed76693abad0405afc2a28305a019
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-30 23:22:33 -0600

    create new KATA returning strings

---
+++ src/Kata/returningStrings.js
@@ -0,0 +1,18 @@
+////CLARIFY THE PROBLEM
+//make a function that will return a greeting statement.
+
+//FUNCTION SIGNATURE
+//function(string) => string
+
+//DUMMY DATA
+// greet("Ben") => "Hello, Ben how are you doing today?"
+
+//PSEUDO CODE
+//step 1: Make an arrow function with the implicit return display the name by using template string with the back ticks.
+
+const greet = (name) => `Hello, ${name} how are you doing today?`
+
+
+greet("Ben");
+
+//PASSED
