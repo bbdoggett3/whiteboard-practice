bd3b2a15353d8d02c7ff876df59981f2eadb9d32
bbdoggett3 <bbdoggett3@gmail.com>
2020-08-30 23:38:45 -0600

    create a helloName KATA

---
+++ src/Kata/helloName.js
@@ -0,0 +1,16 @@
+//CLARIFY THE PROBLEM
+//Returns "Hello, {Name}!" or if no name is passed in then it returns "Hello, World!"
+
+function hello(name) {
+  
+    if(name) {
+    let firstLetter = name.charAt(0).toUpperCase();
+    let noFirstLetter = name.slice(1).toLowerCase();
+      
+      return `Hello, ${firstLetter}${noFirstLetter}!`
+    } else {
+      return `Hello, World!`
+    }
+  }
+
+  //PASSED
