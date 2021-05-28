0267d4c085ba1c40209e9c552d2b32f54fa3201c
bbdoggett3 <bbdoggett3@gmail.com>
2021-05-27 23:13:05 -0600

    working on kata

--- src/Kata/removeExclamation.js
+++ src/Kata/removeExclamation.js
@@ -10,23 +10,15 @@
 //#4 Step 1: create a function called removeExclamationMarks that takes in a string as a paramater
     //Step 2: You will need to loop through the string and check if it has any '!' in the string. If it doesn remove them.
     
-function removeExclamationMarks(string) {
-    // let newString = string;
+function removeExclamationMarks(s) {
+    let newString = s;
 
-    // while (newString[newString.length - 1] === "!") {
-    //     newString = newString.slice(0, -1);
-    // }
-
-    // return newString;
-}
-
-let newString = string;
-
-for(let i = 0; i < newString.length; i++) {
-    if (newString[i] === "i") {
-        newString[i].slice(0,1)
+    while (newString[newString.length - 1] === "!") {
+        newString = newString.slice(0, -1);
     }
+
     return newString;
 }
+
 
 
