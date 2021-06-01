9aac2a0ea6957b95922a2998ee932e6894247f12
bbdoggett3 <bbdoggett3@gmail.com>
2021-05-31 23:10:48 -0600

    worked on kata

---
+++ src/Kata/stringEndsWith.js
@@ -0,0 +1,18 @@
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//Examples:
+
+//solution('abc', 'bc') // returns true
+//solution('abc', 'd') // returns false
+
+//FUNCTION SIGNATURE
+//function(string, string) => string
+
+//NEED TO FINISH KATA
+function solution(str, ending) {
+    for(let i = 0; i < str.length; i++) {
+        if(str.charAt(str.length - 1) === ending) {
+            return true
+        } else return false
+    }
+}
