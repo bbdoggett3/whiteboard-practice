1c2480a7645ea915e798649440e0d8527dbe6cc8
bbdoggett3 <bbdoggett3@gmail.com>
2021-06-09 20:44:04 -0600

    create new array file for coding challenges

---
+++ src/Kata/arrayProblems.js
@@ -0,0 +1,6 @@
+//PROBLEM: Write a program to reverse the array
+
+//CLARIFY: 
+// 1-Just take in an array and output the array reversed?
+// 2-Is this just taking in one array or any other data types.
+// 3- Is there anything in particular that you are looking for
